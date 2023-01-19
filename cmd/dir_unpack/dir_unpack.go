55fdad4c0b4fae69034ce1d47f679d3f9b2c573e
Piotr Halama <skrzynka@halamix2.pl>
2023-01-20 00:16:36 +0100

    Rewrite WAD packing/unpacking to Go (#18)
    * Update linter list
    
    * Rewrite WAD packing/unpacking to Go
    
    * add missing testDir
    
    * fix makefile
    
    * Fix Deepsource findings

---
+++ cmd/dir_unpack/dir_unpack.go
@@ -0,0 +1,90 @@
+/*
+Dir_unpack unpacks .dir/.wad files to directories
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/halamix2/stunt_gp_tools/pkg/dir"
+	"github.com/spf13/pflag"
+)
+
+// flags
+var (
+	outputDirectory string
+)
+
+func parseFlags() {
+	pflag.StringVarP(&outputDirectory, "output", "o", "", "name of the output directory")
+	pflag.Parse()
+}
+
+func usage() {
+	fmt.Println("Unpacks dir archive used by Stunt GP and Worms Armageddon.")
+	fmt.Println("Usage:\ndir_unpack input_file [input_files]")
+	fmt.Println("Flags:")
+	pflag.PrintDefaults()
+}
+
+func main() {
+	parseFlags()
+	args := pflag.Args()
+	if len(args) < 1 {
+		usage()
+		os.Exit(1)
+	}
+
+	if outputDirectory != "" && len(args) > 1 {
+		fmt.Println("Output name can only be used with one input directory")
+		usage()
+		os.Exit(1)
+	}
+
+	for _, inputName := range args {
+		if outputDirectory == "" {
+			// I know, Worms uses .dir extension, but this is primarily Stunt GP program after all
+			outputDirectory = strings.TrimSuffix(inputName, filepath.Ext(inputName))
+		}
+
+		dirFile, err := dir.FromDir(inputName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't read Dir file: %s\n", err)
+			os.Exit(1)
+		}
+
+		err = os.MkdirAll(outputDirectory, 0750)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't create %s path: %s\n", outputDirectory, err)
+			os.Exit(1)
+		}
+
+		err = saveFiles(dirFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't save files: %s\n", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func saveFiles(dirFile dir.Dir) error {
+	for archFilename, archData := range dirFile {
+		archFilename = strings.ReplaceAll(archFilename, "\\", "/")
+		outPath := filepath.Join(outputDirectory, archFilename)
+
+		err := os.MkdirAll(path.Dir(outPath), 0750)
+		if err != nil {
+			return fmt.Errorf("couldn't create %s path: %s", outPath, err)
+		}
+
+		err = os.WriteFile(outPath, archData, 0666)
+		if err != nil {
+			return fmt.Errorf("couldn't save %s file: %s", outPath, err)
+		}
+	}
+	return nil
+}
