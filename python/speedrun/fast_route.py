5807f41bc26b9dcb7552eec6354348b911995ea7
Piotr Halama <skrzynka@halamix2.pl>
2023-03-14 21:21:31 +0100

    Small fixes in speedrun calc

--- python/speedrun/fast_route.py
+++ python/speedrun/fast_route.py
@@ -31,7 +31,7 @@ class PointTime:
         minutes = int(self.time / 60 / 60)
         seconds = int(self.time / 60 - (minutes * 60))
         milliseconds = int((self.time / 60 - (minutes * 60) - seconds) * 100)
-        return "{:02d}:{:02d}.{:02d}".format(minutes, seconds, milliseconds)
+        return f"{minutes:02d}:{seconds:02d}.{milliseconds:02d}"
 
 
 class PointScore:
@@ -42,10 +42,10 @@ class PointScore:
 
 class ArcadePoint:
     def __init__(self):
-        self.lap = PointTime(
-            2 * 60 * 60, "..."
-        )  # default time for "..." player, 2 mins
-        self.total = PointTime(10 * 60 * 60, "...")  # 10 minutes, total race time
+        # default time for "..." player, 2 mins
+        self.lap = PointTime(2 * 60 * 60, "...")
+        # 10 minutes, total race time
+        self.total = PointTime(10 * 60 * 60, "...")
         self.unknown = PointScore(0, "...")
         self.stunts = PointScore(0, "...")
 
@@ -55,7 +55,7 @@ def get_name(name: bytes) -> str:
     return name[:terminator].decode("ascii")
 
 
-def addEdgeIf(graph, start: str, end: str, edge_weight: int):
+def add_edge_if(graph, start: str, end: str, edge_weight: int):
     if edge_weight is None:
         graph.add_edge(start, end, weight=edge_weight)
 
@@ -64,7 +64,7 @@ def convert_time(time: int) -> str:
     minutes = int(time / 60 / 60)
     seconds = int(time / 60 - (minutes * 60))
     milliseconds = int((time / 60 - (minutes * 60) - seconds) * 100)
-    return "{:02d}:{:02d}.{:02d}".format(minutes, seconds, milliseconds)
+    return f"{minutes:02d}:{seconds:02d}.{milliseconds:02d}"
 
 
 def main():
@@ -73,7 +73,7 @@ def main():
 
     points = [None] * 18
     if not os.path.exists(filename) or not os.path.isfile(filename):
-        print("File {} not found".format(filename), file=sys.stderr)
+        print(f"File {filename} not found", file=sys.stderr)
     else:
         # deepcode ignore PT: cli tool
         with open(filename, "rb") as save_file:
@@ -96,10 +96,11 @@ def main():
                     points[i] = point
 
         # make graph
-        # TODO check if weight can be assigned to nodes rather than edges and make that prettier
+        # TODO check if weight can be assigned to nodes rather than edges
+        #  and make that prettier
 
-        G = nx.DiGraph()
-        G.add_nodes_from(
+        g = nx.DiGraph()
+        g.add_nodes_from(
             [
                 "start",
                 "01",
@@ -124,58 +125,56 @@ def main():
             ]
         )
         # 6 -> end points connections
-        addEdgeIf(G, "08", "end", points[0].total.time)
-        addEdgeIf(G, "05", "end", points[1].total.time)
-        addEdgeIf(G, "17", "end", points[2].total.time)
-        addEdgeIf(G, "21", "end", points[3].total.time)
-        addEdgeIf(G, "14", "end", points[4].total.time)
-        addEdgeIf(G, "10", "end", points[5].total.time)
+        add_edge_if(g, "08", "end", points[0].total.time)
+        add_edge_if(g, "05", "end", points[1].total.time)
+        add_edge_if(g, "17", "end", points[2].total.time)
+        add_edge_if(g, "21", "end", points[3].total.time)
+        add_edge_if(g, "14", "end", points[4].total.time)
+        add_edge_if(g, "10", "end", points[5].total.time)
 
         # 5 -> 6 points connections
-        addEdgeIf(G, "19", "08", points[6].total.time)
-        addEdgeIf(G, "19", "05", points[6].total.time)
-        addEdgeIf(G, "18", "05", points[7].total.time)
-        addEdgeIf(G, "18", "17", points[7].total.time)
-        addEdgeIf(G, "15", "17", points[8].total.time)
-        addEdgeIf(G, "15", "21", points[8].total.time)
-        addEdgeIf(G, "16", "21", points[9].total.time)
-        addEdgeIf(G, "16", "14", points[9].total.time)
-        addEdgeIf(G, "01", "14", points[10].total.time)
-        addEdgeIf(G, "01", "10", points[10].total.time)
+        add_edge_if(g, "19", "08", points[6].total.time)
+        add_edge_if(g, "19", "05", points[6].total.time)
+        add_edge_if(g, "18", "05", points[7].total.time)
+        add_edge_if(g, "18", "17", points[7].total.time)
+        add_edge_if(g, "15", "17", points[8].total.time)
+        add_edge_if(g, "15", "21", points[8].total.time)
+        add_edge_if(g, "16", "21", points[9].total.time)
+        add_edge_if(g, "16", "14", points[9].total.time)
+        add_edge_if(g, "01", "14", points[10].total.time)
+        add_edge_if(g, "01", "10", points[10].total.time)
 
         # 4 -> 5 points connections
-        addEdgeIf(G, "07", "19", points[11].total.time)
-        addEdgeIf(G, "07", "18", points[11].total.time)
-        addEdgeIf(G, "22", "18", points[12].total.time)
-        addEdgeIf(G, "22", "15", points[12].total.time)
-        addEdgeIf(G, "09", "15", points[13].total.time)
-        addEdgeIf(G, "09", "16", points[13].total.time)
-        addEdgeIf(G, "04", "16", points[14].total.time)
-        addEdgeIf(G, "04", "01", points[14].total.time)
+        add_edge_if(g, "07", "19", points[11].total.time)
+        add_edge_if(g, "07", "18", points[11].total.time)
+        add_edge_if(g, "22", "18", points[12].total.time)
+        add_edge_if(g, "22", "15", points[12].total.time)
+        add_edge_if(g, "09", "15", points[13].total.time)
+        add_edge_if(g, "09", "16", points[13].total.time)
+        add_edge_if(g, "04", "16", points[14].total.time)
+        add_edge_if(g, "04", "01", points[14].total.time)
 
         # 3 -> 4 points connections
-        addEdgeIf(G, "20", "07", points[15].total.time)
-        addEdgeIf(G, "20", "22", points[15].total.time)
-        addEdgeIf(G, "06", "22", points[16].total.time)
-        addEdgeIf(G, "06", "09", points[16].total.time)
-        addEdgeIf(G, "02", "09", points[17].total.time)
-        addEdgeIf(G, "02", "04", points[17].total.time)
+        add_edge_if(g, "20", "07", points[15].total.time)
+        add_edge_if(g, "20", "22", points[15].total.time)
+        add_edge_if(g, "06", "22", points[16].total.time)
+        add_edge_if(g, "06", "09", points[16].total.time)
+        add_edge_if(g, "02", "09", points[17].total.time)
+        add_edge_if(g, "02", "04", points[17].total.time)
 
         # start -> 3 points connections
-        addEdgeIf(G, "start", "20", 0)
-        addEdgeIf(G, "start", "06", 0)
-        addEdgeIf(G, "start", "02", 0)
+        add_edge_if(g, "start", "20", 0)
+        add_edge_if(g, "start", "06", 0)
+        add_edge_if(g, "start", "02", 0)
 
         # there are 24 paths max from start to end
-        shortests = list(nx.shortest_simple_paths(G, "start", "end", "weight"))
+        shortests = list(nx.shortest_simple_paths(g, "start", "end", "weight"))
         if len(shortests) < 0:
             print("cannot find a path from start to end", file=sys.stderr)
         else:
             if len(shortests) < args.count:
                 print(
-                    "found only {} shortest paths, but {} were requested".format(
-                        len(shortests), args.count
-                    ),
+                    f"found only {len(shortests)} shortest paths, but {args.count} were requested",
                     file=sys.stderr,
                 )
                 # for insurance everything will work fine
@@ -187,26 +186,26 @@ def main():
                 times = []
                 total = 0
                 for current_point in range(1, len(path) - 1):
-                    time = G.get_edge_data(
+                    time = g.get_edge_data(
                         path[current_point], path[current_point + 1]
                     )["weight"]
                     total += time
                     time_string = convert_time(time)
                     times.append(time_string)
-                print("Path #{}:".format(i))
+                print(f"Path #{i}:")
                 # cut start and end points
                 print(" -> ".join(path[1:-1]))
-                print("{} = {}".format(" + ".join(times), convert_time(total)))
+                print(f'{" + ".join(times)} = {convert_time(total)}')
                 print()
 
             print()
-            print("08    05    17    21    14    10")
-            print("  \  /  \  /  \  /  \  /  \  /  ")
-            print("   19    18    15    16    01   ")
-            print("     \  /  \  /  \  /  \  /     ")
-            print("      07    22    09    04      ")
-            print("        \  /  \  /  \  /        ")
-            print("         20    06    02         ")
+            print(r"08    05    17    21    14    10")
+            print(r"  \  /  \  /  \  /  \  /  \  /  ")
+            print(r"   19    18    15    16    01   ")
+            print(r"     \  /  \  /  \  /  \  /     ")
+            print(r"      07    22    09    04      ")
+            print(r"        \  /  \  /  \  /        ")
+            print(r"         20    06    02         ")
             print()
             print(
                 "See https://github.com/Halamix2/stunt_gp_formats/wiki/Tracks to see what track names corresponds to each ID"
