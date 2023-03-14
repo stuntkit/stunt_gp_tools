5f4594d484d6bcb8dcdd642b62692c493f4411e0
Piotr Halama <skrzynka@halamix2.pl>
2023-03-14 21:25:34 +0100

    make Deepsource happy

--- python/calculate_checksum.py
+++ python/calculate_checksum.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
 
-import struct 
+import struct
+
 
 def checksum(data):
-    # originally loop was hardcoded to save size, 0x211C bytes, but we can do this instead
-    hash = 0
-    # data[i] + rotate hash left 3 bits on 32 bits, I hope one crop to 32 bits is enough
-    for i in range(0, len(data)):
-        hash = ( int(data[i]) + ( hash >> 0x1D | hash << 3 ) ) & 0xffffffff
-    return hash
-    
+    """calculate Stunt GP savefile checksum"""
+    hash_val: int = 0
+    # i + rotate hash left 3 bits on 32 bits
+    # I hope one crop to 32 bits is enough
+    for i in data:
+        hash_val = (int(i) + (hash_val >> 0x1D | hash_val << 3)) & 0xFFFFFFFF
+    return hash_val
+
+
 def main():
-    with open('setup.bin', 'rb') as savefile:
+    with open("setup.bin", "rb") as savefile:
         copied = list(savefile.read())
-        expected = struct.unpack("<I",bytes(copied[4:8]))[0]
-        # set checksum to 0 
-        copied[4] = b'0'
-        copied[5] = b'0'
-        copied[6] = b'0'
-        copied[7] = b'0'
+        expected = struct.unpack("<I", bytes(copied[4:8]))[0]
+        # set checksum bytes to 0
+        copied[4] = b"0"
+        copied[5] = b"0"
+        copied[6] = b"0"
+        copied[7] = b"0"
 
         check = checksum(copied)
 
         if expected != check:
             print(f"expected {hex(expected)}, got {hex(check)}")
         else:
-            print('The checksum is correct')
+            print("The checksum is correct")
+
 
 if __name__ == "__main__":
     main()
